e087e494df13ad4dc79d284414625715210fb328
Taras <eostasarat@gmail.com>
2026-09-11 17:47:11 +0300

    Fix dynamic OrderID

--- task.sql
+++ task.sql
@@ -3,10 +3,12 @@ USE ShopDB;
 INSERT INTO Orders (CustomerID, Date)
 VALUES (1, '2023-01-01');
 
+SET @NewOrderID = LAST_INSERT_ID();
+
 START TRANSACTION;
 
 INSERT INTO OrderItems (OrderID, ProductID, Count)
-VALUES (1, 1, 1);
+VALUES (@NewOrderID, 1, 1);
 
 UPDATE Products
 SET WarehouseAmount = WarehouseAmount - 1
